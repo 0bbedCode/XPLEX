c92c0739b6d7d42895077628b7ce2ab77244216d
0bbedCode <114315756+0bbedCode@users.noreply.github.com>
2024-09-20 00:43:07 +0100

    Update UidProcessObserver.java
    Improved the UID Observer code a Bit and added Threaded Options

--- shared/src/main/java/com/obbedcode/shared/usage/monitor/UidProcessObserver.java
+++ shared/src/main/java/com/obbedcode/shared/usage/monitor/UidProcessObserver.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.os.Build;
 import android.os.RemoteException;
 
+import com.obbedcode.shared.GhostCallerUid;
 import com.obbedcode.shared.logger.XLog;
 import com.obbedcode.shared.reflect.DynamicMethod;
 import com.obbedcode.shared.reflect.ReflectUtil;
@@ -12,6 +13,8 @@ import com.obbedcode.shared.reflect.HiddenApiUtils;
 import com.obbedcode.shared.utils.ThreadUtils;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import rikka.hidden.compat.ActivityManagerApis;
 import rikka.hidden.compat.adapter.UidObserverAdapter;
@@ -32,7 +35,9 @@ public class UidProcessObserver {
     private boolean mUseOldMethod = false;
     private UidObserverAdapter mUidObserverAdapter = null;
     private INotifyUid mNotifyUidEvent;
-    private boolean mKeepMonitoring = false;
+    private boolean mIsMonitoring = false;
+
+    private ExecutorService mRunningService = Executors.newSingleThreadExecutor();
 
     public UidProcessObserver(int uid) { mUid = uid; mUseOldMethod = Build.VERSION.SDK_INT == Build.VERSION_CODES.M; }
 
@@ -40,116 +45,131 @@ public class UidProcessObserver {
     public UidProcessObserver setOnNotifyEvent(INotifyUid notifyEvent) { this.mNotifyUidEvent = notifyEvent; return this; }
 
     @SuppressWarnings("unused")
-    public void stopMonitorOnActive() {
-        mKeepMonitoring = false;
+    public void stop() {
+        mIsMonitoring = false;
         if(mUidObserverAdapter != null) {
             try {
                 ActivityManagerApis.unregisterUidObserver(mUidObserverAdapter);
                 mUidObserverAdapter = null;
             }catch (Exception ignored) {  }
         }
+
+        try {
+            mRunningService.shutdownNow();
+            mRunningService.shutdown();
+            mRunningService = Executors.newSingleThreadExecutor();
+        }catch (Exception ignored) { }
     }
 
     @SuppressWarnings("unused")
-    public void startMonitorOnActive() { startMonitorOnActive(true); }
+    public void startAsync(boolean stopWhenFound) {
+        if(!mIsMonitoring) {
+            mRunningService.submit(() -> { start(stopWhenFound); });
+        }
+    }
 
     @SuppressWarnings("unused")
-    public void startMonitorOnActive(boolean stopWhenFound) {
-        if(!mKeepMonitoring) {
-            mKeepMonitoring = true;
-            XLog.i(TAG, "Starting UID monitor for UID: " + mUid + " Use Old: " + mUseOldMethod, true);
+    public void start() { start(true); }
+    public void start(final boolean stopWhenFound) {
+        if(mNotifyUidEvent == null) {
+            XLog.e(TAG, "Notify Event is Null please set the Event so it can be Invoked when UID is found...", true, true);
+            return;
+        }
+
+        if(mUid <= 0 || mUid > 100000) {
+            XLog.e(TAG, "UID is invalid please set a Valid UID between 1 and 10000, UID: " + mUid, true, true);
+            return;
+        }
+
+        if(mIsMonitoring) {
+            XLog.e(TAG, "Cant Monitor for UID when its already monitoring else...", true, true);
+            return;
+        }
+
+        mIsMonitoring = true;
+        XLog.i(TAG, "Starting UID monitor for UID [" + mUid + "] use old method ? " + String.valueOf(mUseOldMethod), true);
+        try {
             if(mUseOldMethod) {
-                try {
-                    Object am = HiddenApiUtils.getIActivityManager();
-                    if(am == null) {
-                        mKeepMonitoring = false;
-                        XLog.e(TAG, "Failed to Get IActivity Manager Class Interface...", true, true);
-                        return;
-                    }
+                Object am = HiddenApiUtils.getIActivityManager();
+                if(am == null) {
+                    XLog.e(TAG, "Failed to get IActivity Manager Class Interface...", true, true);
+                    return;
+                }
 
-                    DynamicMethod mth = new DynamicMethod(am.getClass(), "getRunningAppProcesses")
-                            .bindInstance(am);
+                DynamicMethod mth = new DynamicMethod(am.getClass(), "getRunningAppProcesses")
+                        .bindInstance(am);
 
-                    boolean foundBefore = false;
-                    boolean foundCurrent = false;
-                    int lastFailCount = 0;
-
-                    if(mth.isValid()) {
-                        while (mKeepMonitoring) {
-                            //foundCurrent = false;
-                            List<ActivityManager.RunningAppProcessInfo> runningProcesses = mth.tryInstanceInvoke();
-                            if(!CollectionUtils.isValid(runningProcesses)) {
-                                ThreadUtils.sleep(1000);
-                                lastFailCount++;
-                                if(lastFailCount > 10) {
-                                    mKeepMonitoring = false;
-                                    XLog.e(TAG, "UID Observing Failed...", true, true);
-                                    return;
-                                } continue;
-                            }
-
-                            if(lastFailCount > 0) lastFailCount = 0;
-                            foundCurrent = false;
-                            for(ActivityManager.RunningAppProcessInfo processInfo : runningProcesses) {
-                                if(processInfo.uid == mUid) {
-                                    foundCurrent = true;
-                                    if(!foundBefore) {
-                                        foundBefore = true;
-                                        XLog.i(TAG, "Found UID From Old method: " + mUid, true);
-                                        if(mNotifyUidEvent != null) mNotifyUidEvent.onUidActive(mUid);
-                                        if(stopWhenFound) {
-                                            XLog.i(TAG, "Monitor for UID: " + mUid + " Will stop now...", true);
-                                            mKeepMonitoring = false;
-                                            return;
-                                        }
-                                    } break;
+                if(mth.isValid()) {
+                    new GhostCallerUid().startAction(() -> {
+                        int fails = 0;
+                        boolean wasFound = false;
+                        while (mIsMonitoring && fails < 50) {
+                            try {
+                                boolean hasFound = false;
+                                List<ActivityManager.RunningAppProcessInfo> processes = mth.tryInstanceInvoke();
+                                if(!CollectionUtils.isValid(processes)) {
+                                    XLog.e(TAG, "Collection Returned from [getRunningAppProcesses] returned null or empty...", true, true);
+                                    fails++;
+                                    continue;
                                 }
+
+                                for(ActivityManager.RunningAppProcessInfo process : processes) {
+                                    if(process.uid == mUid) {
+                                        hasFound = true;
+                                        if(!wasFound) {
+                                            wasFound = true;
+                                            if(stopWhenFound) mIsMonitoring = false;
+                                            if(mNotifyUidEvent != null) mNotifyUidEvent.onUidActive(mUid);
+                                        } break;
+                                    }
+                                }
+
+                                if(!hasFound && wasFound) {
+                                    //Meaning it HAS not found the UID BUT it WAS found Before now lets reset the flag
+                                    wasFound = false;
+                                }
+
+                                ThreadUtils.sleep(800);
+                            }catch (Exception innerE) {
+                                XLog.e(TAG, "Error for Monitoring [getRunningAppProcesses] Error: " + innerE.getMessage() + " UID: " + mUid, true, true);
+                                fails++;
                             }
+                        }
+                    });
+                } else {
+                    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                        try {
+                            mUidObserverAdapter = new UidObserverAdapter() {
+                                @Override
+                                public void onUidActive(int uid) throws RemoteException {
+                                    if(mUid == uid) {
+                                        mNotifyUidEvent.onUidActive(uid);
+                                        if(stopWhenFound) mIsMonitoring = false;
+                                        if(!mIsMonitoring) stop();  // ActivityManagerApis.unregisterUidObserver(this);
+                                    }
+                                }
+                            };
 
-                            if(!foundCurrent && foundBefore)
-                                foundBefore = false;//reset last time flag
-
-                            ThreadUtils.sleep(800);
+                            XLog.i(TAG, "Starting UID Observer [registerUidObserver] UID: " + mUid + " [UID_OBSERVER_ACTIVE][" + UID_OBSERVER_ACTIVE + "] [PROCESS_STATE_UNKNOWN][" + PROCESS_STATE_UNKNOWN + "]", true);
+                            ActivityManagerApis.registerUidObserver(
+                                    mUidObserverAdapter,
+                                    UID_OBSERVER_ACTIVE,
+                                    PROCESS_STATE_UNKNOWN,
+                                    null);
+                        }catch (Exception innerE) {
+                            XLog.e(TAG, "Error for Monitoring [registerUidObserver] Error: " + innerE.getMessage() + " UID: " + mUid, true, true);
+                            mIsMonitoring = false;
                         }
                     } else {
-                        mKeepMonitoring = false;
-                        XLog.e(TAG, "Failed to Create Dynamic Method for UID Observing [getRunningAppProcesses]", true, true);
-                    }
-                }catch (Exception e) {
-                    mKeepMonitoring = false;
-                    XLog.e(TAG, "Failed to Use the Old method to Monitor for new UIDs, Error: " + e.getMessage(), true, true);
-                }
-            } else {
-                if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-                    try {
-                        XLog.i(TAG, "Starting UidObserver from Activity Manager UID: " + mUid, true);
-                        mUidObserverAdapter = new UidObserverAdapter() {
-                            @Override
-                            public void onUidActive(int uid) throws RemoteException {
-                                //super.onUidActive(uid);
-                                if(mUid == uid) {
-                                    XLog.i(TAG, "UID Active: " + uid, true);
-                                    mNotifyUidEvent.onUidActive(uid);
-                                    if(stopWhenFound)
-                                        mKeepMonitoring = false;
-                                }
-
-                                if(!mKeepMonitoring) ActivityManagerApis.unregisterUidObserver(this);
-                            }
-                        };
-
-                        XLog.i(TAG, "Registering the UID Observer for Activity Manager UID: " + mUid + " ACTIVE VAL: " + UID_OBSERVER_ACTIVE + " UNKOWN VAL: " + PROCESS_STATE_UNKNOWN, true);
-                        ActivityManagerApis.registerUidObserver(
-                                mUidObserverAdapter,
-                                UID_OBSERVER_ACTIVE,
-                                PROCESS_STATE_UNKNOWN,
-                                null);
-                    }catch (Exception e) {
-                        mKeepMonitoring = false;
-                        XLog.e(TAG, "Failed to use Activity Manager UidObserver, Error: " + e.getMessage(), true, true);
+                        XLog.e(TAG, "Failed to Monitor UID as Android is less than [M] API Level [23], yet Old method has not been selected for Monitoring.", true, true);
+                        mIsMonitoring = false;
                     }
                 }
             }
+        }
+        catch (Exception e) {
+            XLog.e(TAG, "Failed to Monitor UID, internal Exception. Error: " + e.getMessage() + " UID: " + mUid, true, true);
+            mIsMonitoring = false;
         }
     }
 }
